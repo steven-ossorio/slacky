04c9044716e03fd8ecb3c1c21629c59ec9295575
Steven <codebusiness2014@gmail.com>
2018-07-14 17:38:06 -0400

    Attribute validations and using BCrypt for password encription

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,35 @@
 class User < ApplicationRecord
-  validates :username, length: { minimum: 5 }, presence: true
-  validates :email_address, uniqueness: true, presence: true
+  validates :username, :email_address, uniqueness: true
+  validates :username, :email_address, :password_digest, presence: true
+  validates :password, length: { minimum: 6 }
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
