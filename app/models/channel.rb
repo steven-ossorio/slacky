0a77e887a552717d893d74b9ce30033a169b88a5
Steven <codebusiness2014@gmail.com>
2018-07-15 21:12:25 -0400

    Added message association in Channel model

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -5,4 +5,9 @@ class Channel < ApplicationRecord
   foreign_key: :workspace_id,
   class_name: :Workspace,
   primary_key: :id
+
+  has_many :messages,
+  class_name: :Message,
+  foreign_key: :channel_id,
+  primary_key: :id
 end
