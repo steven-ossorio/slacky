c38d990e409fea7353d1dfd888c56bfc4d0eea56
Steven <codebusiness2014@gmail.com>
2018-07-15 20:48:43 -0400

    Create association between workspace and member table

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,3 +1,8 @@
 class Workspace < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+
+  has_many :members,
+  class_name: :Member,
+  foreign_key: :workspace_id,
+  primary_key: :id
 end
