873a993d05ea0a4540d98f2ef297506208951c8f
Steven <codebusiness2014@gmail.com>
2018-07-15 20:47:30 -0400

    Added user_id column to workspace table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_14_221845) do
+ActiveRecord::Schema.define(version: 2018_07_16_004700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "members", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "workspace_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -27,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_07_14_221845) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
 end
