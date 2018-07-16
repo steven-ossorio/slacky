a234bbe2b095f8c3cc49294cf76df4597bafb298
Steven <codebusiness2014@gmail.com>
2018-07-15 20:05:42 -0400

    Add association between User, Member and Workspace

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,11 @@
 class Member < ApplicationRecord
+  has_many :users,
+  foreign_key: :user_id,
+  class_name: :User,
+  primary_key: :id
+
+  has_many :workspaces,
+  foreign_key: :workspace_id,
+  class_name: :Workspace,
+  primary_key: :id
 end
