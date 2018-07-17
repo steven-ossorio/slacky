f884fa0c08b247f49c220a71f9ecede47f85b5ed
Steven <codebusiness2014@gmail.com>
2018-07-17 12:57:06 -0400

    Regenerated Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_16_010908) do
+ActiveRecord::Schema.define(version: 2018_07_17_011346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_07_16_010908) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "session_token"
   end
 
   create_table "workspaces", force: :cascade do |t|
