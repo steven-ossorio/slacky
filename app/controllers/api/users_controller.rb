0cd89423a6a91430a1f0dd61c5d3fbcc37ee826d
Steven <codebusiness2014@gmail.com>
2018-08-02 16:37:20 -0400

    Successfully add a user through postman

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,23 @@
-class API::UsersController < ApplicationController
+class Api::UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: @user.errors.messages, status: 422
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email_address, :password, :username)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#root'
+
+  namespace :api, defaults: { format: :json } do
+    post 'register', to: 'users#create'
+    get 'user/:id', to: 'users#show'
+  end
 end
