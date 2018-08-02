27541b064d0a8c41d6322b0a6b09a574add5accd
Steven <codebusiness2014@gmail.com>
2018-08-02 19:34:52 -0400

    Added association between workshop and channels models

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -41,5 +41,10 @@ class Workspace < ApplicationRecord
   foreign_key: :workspace_id,
   primary_key: :id
 
+  has_many :channels,
+  class_name: :Channel,
+  foreign_key: :workspace_id,
+  primary_key: :id
+
   has_many :members, through: :memberships, source: :user
 end
