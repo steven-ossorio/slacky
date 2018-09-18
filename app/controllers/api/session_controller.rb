7a48716038d46b6b80e933de719ff5ddfc7a393c
Steven <codebusiness2014@gmail.com>
2018-09-18 15:24:50 -0400

    Modifying changes

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,18 +1,9 @@
 class Api::SessionController < ApplicationController
-<<<<<<< HEAD
 
   def create
     @user = User.find_by_credentials(
       user_params[:email_address],
       user_params[:password]
-=======
-  skip_before_action :verify_authenticity_token
-
-  def create
-    @user = User.find_by_credentials(
-      params[:user][:email_address],
-      params[:user][:password]
->>>>>>> 7829a44ef8c65ea243e413ae6c4a53a729acac5c
     )
 
     if @user
@@ -22,6 +13,7 @@ class Api::SessionController < ApplicationController
       render json: ['Invalid Credentials'], status: 401
     end
   end
+  
 
   def destroy
     @user = current_user
@@ -32,11 +24,8 @@ class Api::SessionController < ApplicationController
       render json: ['No user is logged in']
     end
   end
-<<<<<<< HEAD
 
   def user_params
     params.require(:user).permit(:email_address, :password)
   end
-=======
->>>>>>> 7829a44ef8c65ea243e413ae6c4a53a729acac5c
 end
