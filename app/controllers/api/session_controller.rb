7fdeca83165c4110c70df025b09143bbb9f6560f
Steven <codebusiness2014@gmail.com>
2018-09-19 11:43:37 -0400

    Accepted changes

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,18 +1,9 @@
 class Api::SessionController < ApplicationController
-<<<<<<< HEAD
 
   def create
     @user = User.find_by_credentials(
       user_params[:email_address],
       user_params[:password]
-=======
-  skip_before_action :verify_authenticity_token
-
-  def create
-    @user = User.find_by_credentials(
-      params[:user][:email_address],
-      params[:user][:password]
->>>>>>> e9b6e61ceba75a328238be5489170a71961ebc51
     )
 
     if @user
@@ -32,11 +23,8 @@ class Api::SessionController < ApplicationController
       render json: ['No user is logged in']
     end
   end
-<<<<<<< HEAD
 
   def user_params
     params.require(:user).permit(:email_address, :password)
   end
-=======
->>>>>>> e9b6e61ceba75a328238be5489170a71961ebc51
 end
