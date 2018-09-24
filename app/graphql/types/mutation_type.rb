64822f44d3180554f8297c4b2def9d05a6f13b4a
Steven <codebusiness2014@gmail.com>
2018-09-24 19:02:42 -0400

    Create channel mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :createUser, function: Resolvers::CreateUser.new
+    field :createChannel, function: Resolvers::CreateChannel.new
   end
 end
