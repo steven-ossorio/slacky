8af9ffe4a3ee4a173e526dd353ae32aeb657da36
Steven <codebusiness2014@gmail.com>
2018-09-27 15:25:10 -0400

    Finished landing page

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default () => {
   return (
-    <div>
-      <div className="Navbar-container">
-        <span>#</span>
-        <span>Slacky</span>
+    <div className="Navbar-container">
+      <div className="Navbar-container-Nav">
+        <Link to="/">
+          <div className="Navbar-container-Nav-Logo">
+            <i class="fa fa-slack" aria-hidden="true" />
+            <span>Slacky</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
