43c83c68b7c1e88ab21af60a20e15df58d71eda2
Steven <codebusiness2014@gmail.com>
2018-10-26 12:26:53 -0400

    Changes to how information is being returned

--- app/graphql/resolvers/create_workspace.rb
+++ app/graphql/resolvers/create_workspace.rb
@@ -1,13 +1,12 @@
 class Resolvers::CreateWorkspace < GraphQL::Function
   argument :name, !types.String
-  argument :user_id, !types.ID
 
   type Types::WorkspaceType
 
-  def call(_obj, args, _ctx)
+  def call(_obj, args, ctx)
     Workspace.create!(
       name: args[:name],
-      user_id: args[:user_id]
+      user: ctx[:current_user]
     )
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,4 +4,6 @@ Types::UserType = GraphQL::ObjectType.define do
   field :id, !types.ID
   field :username, !types.String
   field :email, !types.String
+  # Want to return an array of workspaces current_user is associated with.
+  field :workspaces, -> { !types[Types::WorkspaceType] }
 end
