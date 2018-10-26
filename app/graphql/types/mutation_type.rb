090d16070841097cd219fd1b0a9b2ee4abc710ab
Steven <codebusiness2014@gmail.com>
2018-10-26 16:27:53 -0400

    Logout a user mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,11 +2,12 @@ module Types
   class MutationType < Types::BaseObject
     field :createUser, function: Resolvers::CreateUser.new
     field :signinUser, function: Resolvers::SignInUser.new
+    field :signoutUser, function: Resolvers::SignOutUser.new
 
     field :createMessage, function: Resolvers::CreateMessage.new
 
     field :createWorkspace, function: Resolvers::CreateWorkspace.new
-    
+
     field :createChannelMember, function: Resolvers::CreateChannelMember.new
 
     # field :createChannel, function: Resolvers::CreateChannel.new
