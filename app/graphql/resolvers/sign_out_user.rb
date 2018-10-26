e4a5a1c5c9e710472f8d84a2e52039f0ffccbcb7
Steven <codebusiness2014@gmail.com>
2018-10-26 16:27:53 -0400

    Logout a user mutation

--- app/graphql/resolvers/sign_out_user.rb
+++ app/graphql/resolvers/sign_out_user.rb
@@ -1,5 +1,16 @@
 class Resolvers::SignOutUser < GraphQL::Function
+  type Types::UserType
+
   def call(_obj, _args, ctx)
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+
+    session_token = crypt.decrypt_and_verify(ctx[:session][:session_token])
+    user_id = session_token.gsub('user-id:', '').to_i
+    user = User.find_by(id: user_id)
+    user.reset_session_token!
+
     ctx[:session][:session_token] = nil
+
+    user
   end
 end
