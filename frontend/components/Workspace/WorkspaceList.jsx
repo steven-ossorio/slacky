a74748abfba3d6b7c5db7a47fbd546a8d9ecef0f
Steven <codebusiness2014@gmail.com>
2018-10-30 15:56:04 -0400

    Finish styling dropdown and logout button

--- frontend/components/Workspace/WorkspaceList.jsx
+++ frontend/components/Workspace/WorkspaceList.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import styles from "./WorkspaceList.scss";
 
 const WorkspaceList = props => {
   if (props.workspaces.length === 0) {
     return <div>Current Not Joined to any Workspace.</div>;
   }
-  return <div />;
+  let workspaces = props.workspaces.map(workspace => {
+    return (
+      <li className={styles.workspaceList} key={workspace.id}>
+        {workspace.name}
+      </li>
+    );
+  });
+
+  return <ul className={styles.workspaceListContainer}>{workspaces}</ul>;
 };
 
 export default WorkspaceList;
